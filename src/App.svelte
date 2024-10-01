b1d7d1164d9114e3fb6c0a26c7d0586fe7b67759
Jurn Wubben <46420489+jsw08@users.noreply.github.com>
2024-10-01 11:27:33 +0000

    Implemented timer logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import ICAL from "ical.js";
 
-  interface Lessons {
+  type Lessons = {
     summary: string;
     endDate: Date;
-  }
+  };
+  type Timers = {
+    name: string;
+    time: `${number}:${number}:${number}`;
+  };
 
-  const getCalendar = async (): Promise<Lessons[]> => {
+  const getLessons = async (): Promise<Lessons[]> => {
     const response = await fetch(
       "https://elo.somtoday.nl/services/webdav/calendarfeed/d1b640b7-b7da-4aee-a10f-e259a49fcdc5/6a3e961d-f875-4033-a927-fb851092a45b",
     );
@@ -32,20 +36,41 @@
     );
     return todayLessons;
   };
+
+  const calculateTimeLeft = (endDate: Date): `${number}:${number}:${number}` => {
+    const now = new Date();
+    const diff = endDate.getTime() - now.getTime();
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    return `${hours}:${minutes}:${seconds}`;
+  };
+  const calculateAllTimesLeft = (lessons: Lessons[], max: number = 2, all: boolean = false) => (all ? lessons : lessons.slice(0, max)).map((lesson) => ({name: lesson.summary, time: calculateTimeLeft(lesson.endDate)}));
+
+  let lessons: Lessons[] = [];
+  let timers: Timers[] = $state([]);
+
+  $effect(() => {
+    getLessons().then(v => lessons = v)
+    const calendarInterval = setInterval(() => {alert("UPDATED");getLessons().then(v => lessons = v)}, 900000); // 900000
+
+    const timersInterval = setInterval(() => {
+      timers = calculateAllTimesLeft(lessons);
+    }, 1000);
+
+    return () => {
+      clearInterval(calendarInterval);
+      clearInterval(timersInterval);
+    };
+  });
+  
+
 </script>
 
 <main class="w-full h-screen flex justify-center items-center">
   <div
-    class="p-4 lg:p-8 border-2 rounded-xl shadow-xl hover:shadow-lg hover:translate-y-1 transition-all flex justify-center items-center text-[4rem] md:text-[10rem] xl:text-[15rem] font-mono font-gray"
+    class="p-4 lg:p-8 border-2 rounded-xl shadow-xl hover:shadow-lg hover:translate-y-1 transition-all flex justify-center items-center text-[4rem]  font-mono font-gray"
   >
-    20:00
+    {JSON.stringify(timers)}
   </div>
 </main>
-
-{#await getCalendar()}
-  <p>Loading...</p>
-{:then jcalData}
-  <p>{JSON.stringify(jcalData)}</p>
-{:catch error}
-  <p style="color: red">{error.message}</p>
-{/await}
